714d1db4ef37d0d40a7f13feb224444fb7dbe0b0
zaleskiy88 <zaleskiy88@gmail.com>
2022-06-05 19:38:36 +0100

    updated fetching functions

--- src/movie-fetch.js
+++ src/movie-fetch.js
@@ -2,52 +2,63 @@ const axios = require('axios').default;
 
 const API_KEY = '842344de8347536aefc6f17e8e76d4bd';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie/`;
-const TRENDING_URL = 'https://api.themoviedb.org/3/trending/movie/day';
+const TRENDING_URL = `https://api.themoviedb.org/3/trending/movie/day`;
 
-let page = 1;
-let moviesPage = 1;
-let searchQueryStr = '';
+const parameters = { page: 1, moviesPage: 1, searchQueryStr: '' };
 
 //первый запрос для поиска фильма по-имени
 export async function getDataMovies(searchQuery) {
-  searchQueryStr = searchQuery;
+  parameters.searchQueryStr = searchQuery;
 
-  const response = await axios.get(
-    `${SEARCH_URL}?api_key=${API_KEY}&query=${searchQuery}&page=1`
-  );
+  const response = await axios.get(`${SEARCH_URL}`, {
+    params: {
+      api_key: API_KEY,
+      query: searchQuery,
+      page: 1,
+    },
+  });
 
-  const dataResults = await response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
-
-  return dataResults;
+  return await response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
 }
 
-//
 export async function getMoreDataMovies(searchQuery) {
-  moviesPage =
-    searchQueryStr === searchQuery ? (moviesPage += 1) : (moviesPage = 1);
+  parameters.moviesPage =
+    parameters.searchQueryStr === searchQuery
+      ? (parameters.moviesPage += 1)
+      : (parameters.moviesPage = 1);
 
-  const response = await axios.get(
-    `${SEARCH_URL}?api_key=${API_KEY}&query=${searchQuery}&page=${moviesPage}`
-  );
-  const dataResults = response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
+  const response = await axios.get(`${SEARCH_URL}`, {
+    params: {
+      api_key: API_KEY,
+      query: searchQuery,
+      page: parameters.moviesPage,
+    },
+  });
 
-  return dataResults;
+  return response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
 }
 
 //первый запрос популярных фильмов
 export async function getTrendingMoviesData() {
-  const response = await axios.get(`${TRENDING_URL}?api_key=${API_KEY}`);
-  const dataResults = await response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
+  const response = await axios.get(`${TRENDING_URL}`, {
+    params: {
+      api_key: API_KEY,
+      page: parameters.page,
+    },
+  });
 
-  return dataResults;
+  return await response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
 }
 
 export async function getMoreTrendingMoviesData() {
-  page += 1;
-  const response = await axios.get(
-    `${TRENDING_URL}?api_key=${API_KEY}&page=${page}`
-  );
-  const dataResults = await response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
+  parameters.page += 1;
 
-  return dataResults;
+  const response = await axios.get(`${TRENDING_URL}`, {
+    params: {
+      api_key: API_KEY,
+      page: parameters.page,
+    },
+  });
+
+  return await response.data.results; // возвращает объект с данными фильмов. Для того чтоб достучатся к свойствам page, total_pages, total_results нужно обратится к response.data
 }
