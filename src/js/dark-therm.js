73d7ad13e251874cf6ff4cbeaeae76b421d610e9
Fefaodessa <fefaodessa@gmail.com>
2022-06-07 00:10:58 +0300

    dark

--- src/js/dark-therm.js
+++ src/js/dark-therm.js
@@ -1,8 +1,33 @@
 
-
 const btnTheme = document.querySelector('#dark-theme');
 const body = document.querySelector('.body');
 
+const localTheme = localStorage.getItem('color-theme');
+
+if (localTheme ==="dark-theme") {
+    body.classList.add('dark-theme');
+    /* btnTheme.checked = true; */
+}
+
 btnTheme.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
+    switchThemeColor() ;
 });
+
+function switchThemeColor() {
+if (document.querySelector('.dark-theme')){
+    localStorage.setItem('color-theme','dark-theme'); // сохраняем
+}else {
+    localStorage.removeItem('color-theme'); // удаляем
+}
+}
+
+/* const onPlay = function(data) {
+    const time = data.seconds
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+    
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const timeStopPlayer = localStorage.getItem("videoplayer-current-time");
+const timeStop =JSON.parse(timeStopPlayer) */
