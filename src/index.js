5c1f0ec114a0e3c176240601fe3257785dcd8e70
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-06-06 12:26:45 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import {
   getDataMovies,
   getMoreMoviesData,
-  getTrendingMovies,
-  getMoreTrendingMovies,
+  getMoreTrendingMoviesData,
+  getTrendingMoviesData,
 } from './movie-fetch';
+
+const mainGallery = document.querySelector("#home-gallery");
+
+async function generateMurkup() {
+  const data = await getTrendingMoviesData();
+  const markup = data.results.map(el => {
+    console.log(el);
+    return `<p>${el.original_title}</p> <img src="https://image.tmdb.org/t/p/w500/${el.backdrop_path}">`
+  })
+  mainGallery.innerHTML = markup.join("");
+}
+
+generateMurkup();
+
+
