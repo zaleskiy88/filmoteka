ed3a9c30906bfd54bc9d5ae9c1e19217aee6d77b
Vebster <vebster.khm@gmail.com>
2022-06-07 15:47:30 +0300

    логика запроса + шаблон hbs для фильма

--- src/movie-fetch.js
+++ src/movie-fetch.js
@@ -1,3 +1,5 @@
+import itemsTemplate from './templates/card-of-list.hbs';
+
 const axios = require('axios').default;
 
 const API_KEY = '842344de8347536aefc6f17e8e76d4bd';
@@ -5,7 +7,54 @@ const SEARCH_URL = `https://api.themoviedb.org/3/search/movie/`;
 const TRENDING_URL = `https://api.themoviedb.org/3/trending/movie/day`;
 
 const parameters = { page: 1, moviesPage: 1, searchQueryStr: '' };
+// ! ------------------------------------------------------------------------------------------
+async function getGenresIds() {
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/genre/movie/list?api_key=842344de8347536aefc6f17e8e76d4bd&language=en-US'
+  );
 
+  //console.log('genres :>> ', genres.data.genres);
+  return response.data.genres;
+}
+
+//===========================================
+const gallery = document.querySelector('.gallery');
+
+async function galleryMarkup() {
+  const moviesData = await getTrendingMoviesData();
+  const genres = await getGenresIds();
+  // console.log('moviesData :>> ', moviesData.results[0]);
+
+  const movieCategories = moviesData.results.map(movie => {
+    const catArr = [];
+    const movieInfo = {
+      name: movie.title,
+      release: movie.release_date,
+      id: movie.id,
+      genres: catArr,
+      poster_path: movie.poster_path,
+      backdrop_path: movie.backdrop_path,
+    };
+
+    movie.genres = movie.genre_ids.map(id =>
+      genres.find(el => {
+        if (el.id === id) {
+          return catArr.push(el.name);
+        }
+      })
+    );
+
+    return movieInfo;
+  });
+
+  console.log('movieCategories :>> ', movieCategories);
+
+  const markup = itemsTemplate(movieCategories);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+galleryMarkup();
+
+// ! ------------------------------------------------------------------------------------------
 //первый запрос для поиска фильма по-имени
 export async function getDataMovies(searchQuery) {
   parameters.searchQueryStr = searchQuery;
