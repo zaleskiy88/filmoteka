7612045f07937c1211c881e2c6ef196b9bf936f0
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-06-07 20:45:58 +0300

    updated markup

--- src/index.js
+++ src/index.js
@@ -3,16 +3,50 @@ import {
   getMoreDataMovies,
   getTrendingMoviesData,
   getMoreTrendingMoviesData,
+  getGenresIds,
 } from './js/movie-fetch';
-const mainGallery = document.querySelector("#home-gallery");
+
+import itemsTemplate from './templates/list-of-card.hbs';
+
+const gallery = document.querySelector('.gallery');
 
 async function generateMurkup() {
-  const data = await getTrendingMoviesData();
-  const markup = data.results.map(el => {
-    console.log(el);
-    return `<p>${el.original_title}</p> <img src="https://image.tmdb.org/t/p/w500/${el.backdrop_path}">`
-  })
-  mainGallery.innerHTML = markup.join("");
+  const moviesData = await getTrendingMoviesData();
+  const genres = await getGenresIds();
+
+  // Создание объекта в котором хранится информация для Handlebars
+  const movieCategories = moviesData.results.map(movie => {
+    const catArr = [];
+    const dataRelease = movie.release_date.slice(0, 4);
+    const nameOfFilm = movie.title.toUpperCase();
+    const movieInfo = {
+      name: nameOfFilm,
+      release: dataRelease,
+      id: movie.id,
+      genres: catArr,
+      poster_path: movie.poster_path,
+      backdrop_path: movie.backdrop_path,
+    };
+
+    // Сравнивает жанры из массива жанров с id жанров из общего запроса и добавляет нужные в объект для Handlebars
+    const genresFilm = function () {
+      movie.genre_ids.map(id =>
+        genres.find(el => {
+          if (el.id === id) {
+            return catArr.push(el.name);
+          }
+        })
+      );
+    };
+
+    genresFilm();
+    return movieInfo;
+  });
+
+  // Создание разметки
+gallery.insertAdjacentHTML('beforeend', itemsTemplate(movieCategories));
 }
+
+
 
 generateMurkup();
