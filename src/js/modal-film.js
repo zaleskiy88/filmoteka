67c7ad44e97980c58e884b1ab9b87cc08d195204
Liubov1102 <laav.odessa@gmail.com>
2022-06-08 13:48:17 +0300

    com
    com

---
+++ src/js/modal-film.js
@@ -0,0 +1,18 @@
+import modalFilm from './templates/modal-film.hbs';
+
+import * as basicLightbox from 'basiclightbox';
+
+
+function onGalleryItems(evt) {
+    evt.preventDefault();
+}
+    const instance = basicLightbox.create(
+        document.querySelector('template'),
+
+        { onShow: () => { window.addEventListener('keydown', onKeydown) } },
+        { onClose: () => { window.removeEventListener('keydown', onKeydown) } } ,
+    );
+ 
+    instance.show();
+ 
+
