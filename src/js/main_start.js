4a913f71cce1466f7f9d30402f24853a64add23b
Fefaodessa <fefaodessa@gmail.com>
2022-06-10 00:02:33 +0300

    main-img

--- src/js/main_start.js
+++ src/js/main_start.js
@@ -1,10 +1,16 @@
-const main = document.querySelector('.main');
+const main = document.querySelector('.main-start');
 const half = document.querySelector('.half');  
 const overlay = document.querySelector('.overlay');
 const buttonLine = document.querySelector('.button-line');
 const superButton = document.querySelector('.super-button');
 const bottomImg = document.querySelector('.bottom')
 const lineright = document.querySelector('.right');
+
+const mainStart = sessionStorage.getItem('main-display')|| "";
+
+if (mainStart !== "") {
+    main.style.display = 'none';
+}
 
 superButton.addEventListener('click', () => {
     bottomImg.classList.add('active');
@@ -14,8 +20,8 @@ superButton.addEventListener('click', () => {
     overlay.classList.add('active');
     buttonLine.classList.add('active');
     superButton.classList.add('active');
+    sessionStorage.setItem('main-display', 'none')
     setTimeout(() => {
-        overlay.display = 'none';
         main.style.display = 'none';
     }, 2500);
 });
