af463e1b04e04b0c68e5a82c8f4a3af7d7be1d23
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-06-09 19:49:04 +0300

    присоединился к логике структуры

--- src/js/api/firebase/index.js
+++ src/js/api/firebase/index.js
@@ -1,6 +1,7 @@
 import auth from './auth_firebase';
 import add from './firebase_write_db';
+import get from './firebase_read_db';
 
 
 // export default { logInByGoogle, logOut, addDocument, getDocument, removeFromQueue, addToQueue, addToWatched };
-export default { auth, add }
+export default { auth, add, get }
