d55653452f0f6d3b8341e83a92f48b74e2639e9e
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-06-09 14:13:47 +0300

    Update modal-film.js

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,14 +1,14 @@
 import modalFilm from '../templates/modal-film.hbs';
 import SimpleLightbox from "simplelightbox";
 //import "simplelightbox/dist/simpleLightbox.min.css";
-import { getDataMovie } from '../js/movie-fetch';
+import { getDataMovies } from './movie-fetch';
 
 
 export const initLightbox = async (e) => {
     e.preventDefault();
     const { id: movieId } = e.currentTarget.dataset;
     const movieModal = new SimpleLightbox();
-    const dataMovie = await getDataMovie(movieId);
+    const dataMovie = await getDataMovies(movieId);
     const markup = await modalFilm([dataMovie])
     movieModal.setContent(markup).show();
 }
