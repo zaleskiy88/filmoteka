1c5edc7f5aca373206e108d141b91b166d177749
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-06-11 11:57:55 +0300

    updated dots and arrows

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,4 @@
-import {getMoreTrendingMoviesData, getGenresIds,} from "./movie-fetch";
+import {getMoreTrendingMoviesData, generateMoviesWithGenres} from "./movie-fetch";
 import itemsTemplate from '../templates/list-of-card.hbs';
 const refs = {
     paginationList: document.querySelector(".pagination-list"),
@@ -8,6 +8,7 @@ const gallery = document.querySelector('.gallery');
 const preloaderContainer = document.querySelector(".preloader");
 
 const maxPage = 20;
+let currentPage = 1;
 
 const pagesArray = Array.apply(null, {
   length: maxPage ?? 0,
@@ -16,22 +17,38 @@ const pagesArray = Array.apply(null, {
   .map((item) => item + 1);
 
   function renderSpan(value) {
-    return `<span>${value}</span>`; 
+    return `<span data-value='${value}'>${value}</span>`; 
   }
 
 refs.paginationList.addEventListener('click', onPaginationBtnClick);
 
 async function onPaginationBtnClick(event) {
-    renderingPaginationMarkup(Number(event.target.textContent));
-    const data = await getMoreTrendingMoviesData(Number(event.target.textContent));
-    const movieCategories = await generateMoviesWithGenres(data.results);
-
-  // Rendering markup
-  setTimeout(() => {
-    preloaderContainer.innerHTML = '';
-    gallery.innerHTML= itemsTemplate(movieCategories);
-    footer.style.position = "static";
-  }, 2000)
+  event.target.classList.add("pagination_active")
+    if (event.target.dataset.span === "prev") {
+      currentPage -= 1;
+      renderingFilmsMarkup(currentPage);
+        return;
+    }
+    if (event.target.dataset.span === "next") {
+      currentPage += 1;
+      renderingFilmsMarkup(currentPage);
+      return;
+    }
+    if (event.target.dataset.value === "dots") {
+      if (Number(event.target.nextElementSibling?.dataset?.value) === maxPage)
+      {
+        currentPage += 1;
+        renderingFilmsMarkup(currentPage);
+        return;
+      }
+      else {
+        currentPage -= 1;
+        renderingFilmsMarkup(currentPage);
+      return;
+      }
+    }
+    currentPage = Number(event.target.textContent);
+    renderingFilmsMarkup(currentPage);
 }
 
 function renderingPaginationMarkup(currentPage) {
@@ -51,56 +68,31 @@ function renderingPaginationMarkup(currentPage) {
           return renderSpan(item);
         }
         if(item === currentPage - 3 || item === currentPage + 3) {
-          return "<span>...</span>";
+          return "<span data-value='dots'>...</span>";
         }
         return "";
            
         }).join("");
         if (currentPage > 1) {
-          result = "<span><=</span>" + result;
+          result = "<span data-span='prev'><=</span>" + result;
         }
         if (currentPage >= 1 && currentPage !== maxPage) {
-          result = result + "<span>=></span>";
+          result = result + "<span data-span='next'>=></span>";
         }
-
-
     refs.paginationList.innerHTML = result;
-    console.log(result);
 }
 
 renderingPaginationMarkup(1);
 
-async function generateMoviesWithGenres(data) {
-  const genres = await getGenresIds();
+async function renderingFilmsMarkup(currentPage) {
+  renderingPaginationMarkup(currentPage);
+      const data = await getMoreTrendingMoviesData(currentPage);
+      const movieCategories = await generateMoviesWithGenres(data.results);
 
-  // Creating an object that stores data for handlebars template
-  return data.map(movie => {
-    const catArr = [];
-    const dataRelease = movie.release_date?.slice(0, 4) || 2022;
-    const nameOfFilm = movie.title.toUpperCase();
-    const movieInfo = {
-      name: nameOfFilm,
-      release: dataRelease,
-      id: movie.id,
-      genres: catArr,
-      poster_path: movie.poster_path,
-      backdrop_path: movie.backdrop_path,
-    };
-
-    // Comparing ganres taken from the general ganre array with IDs and adding ganres' values in the object for handlebars template
-    const genresFilm = function () {
-      movie.genre_ids.map(id =>
-        genres.find(el => {
-          if (el.id === id) {
-            return catArr.push(el.name);
-          }
-        })
-      );
-    };
-
-    genresFilm();
-    return movieInfo;
-  });
+  // Rendering markup
+        setTimeout(() => {
+          preloaderContainer.innerHTML = '';
+          gallery.innerHTML= itemsTemplate(movieCategories);
+          footer.style.position = "static";
+        }, 2000);
 }
-
-  
