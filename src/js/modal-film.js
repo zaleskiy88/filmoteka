c012dca2cd238f64beb4efefc3804c73bd26b31e
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-06-11 14:09:08 +0300

    modal updated

--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,17 +1,40 @@
 import modalFilm from '../templates/modal-film.hbs';
 
-import { getDataMovies } from './movie-fetch';
+import { getDataMovies, getOneMovieById } from './movie-fetch';
 
 
-export const initLightbox = async (e) => {
+const gallery = document.querySelector(".gallery");
+const backdrop = document.querySelector(".backdrop-film");
+gallery.addEventListener('click', onModalClick);
+backdrop.addEventListener('click', onBackdropClick);
+
+function onBackdropClick(event) {
+  console.log("backdrop");
+  console.log(event.target);
+  console.log(event.currentTarget);
+  if(event.target === event.currentTarget) {
+    backdrop.classList.add("visually-hidden");
+  }
 }
 
+async function onModalClick(event) {
+  if(event.target.nodeName !== "IMG") {
+    return;
+  }
+  const movieId = event.target.dataset.id;
+  const dataMovie = await getOneMovieById(movieId);
+  const markup = modalFilm(dataMovie);
+  backdrop.innerHTML = markup;
+  backdrop.classList.remove("visually-hidden");
+}
 
-function onEscKeyPress(event) {
+gallery.addEventListener('keydown', onEscKeyDown)
+function onEscKeyDown(event) {
   const ESC_KEY_CODE = 'Escape';
   const isEscKey = event.code === ESC_KEY_CODE;
 
   if (isEscKey) {
-    onCloseModal();
+    backdrop.classList.add("visually-hidden");
+    gallery.removeEventListener('keydown', onEscKeyDown)
   }
 }
