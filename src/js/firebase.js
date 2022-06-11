af5b3b56c410394367a50b8a5e70684c39afeece
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-06-11 16:17:51 +0300

    20220611

---
+++ src/js/firebase.js
@@ -0,0 +1,84 @@
+import { async } from "@firebase/util";
+import { initializeApp } from "firebase/app";
+import { collection, addDoc, getDocs, getFirestore, doc, where, query, getDoc } from "firebase/firestore"; 
+import Notiflix from 'notiflix';
+import itemsTemplate from '../templates/list-of-card.hbs';
+
+const axios = require('axios').default;
+const API_KEY = '842344de8347536aefc6f17e8e76d4bd';
+const MOVIE_URL = `https://api.themoviedb.org/3/movie/`;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAN-5I6EVm1JCDsJFVlHPFOnqDlUOfsbaw",
+  authDomain: "filmoteka-98189.firebaseapp.com",
+  databaseURL: "https://filmoteka-98189-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "filmoteka-98189",
+  storageBucket: "filmoteka-98189.appspot.com",
+  messagingSenderId: "437648861220",
+  appId: "1:437648861220:web:0545ac1cdbb5bb833190b1"
+};
+const app = initializeApp(firebaseConfig);                              // Initialize Firebase
+const db = getFirestore(app);                                           // Initialize Cloud Firestore and get a reference to the service
+const btnWatched = document.querySelector('.header-library__btnWatc');  // Select Watched button
+const btnQueue = document.querySelector('.header-library__btnQue');     // Select Queue button
+const galleryLibrary = document.querySelector('#library-gallery');
+let usersList = [];
+
+btnWatched.addEventListener('click', onLibraryBtnClick);                // Set the listener on Watch button
+btnQueue.addEventListener('click', onLibraryBtnClick);                  // Set the listener on Queque button
+
+
+// -------------------------- handle button click
+async function onLibraryBtnClick(event) {
+    await getUsersMovieList(event);                                     // getting user's movie list
+    generateLibraryMarkup();                                            // generate maurkup
+}
+
+// -------------------------- getting user's movie list 
+async function getUsersMovieList(event) {
+
+    event.preventDefault();                                             // block reload page
+    const id = '2i1T2rjd9xPzQ3BNETY8W5RQGLl1';                          // ????get user id????
+    const typeOfList = event.target.textContent;                        // what kind of list (queue or watched)
+    const usersCollection = doc(db, 'users', id);                       // forming a querry
+
+    try {
+        const docSnap = await getDoc(usersCollection);                  // send querry
+        usersList = (typeOfList === 'WATCHED') ? docSnap.data().watched : docSnap.data().queue;    // get array with movie list
+
+        if (usersList.length === 0) {
+            Notiflix.Notify.failure(`У вас отсутствуют фильмы в разделе ${typeOfList}`);
+        }
+        return usersList;
+    }
+    catch {
+        Notiflix.Notify.failure(`Ой, что-то пошло не так... Пожалуйста, повторите попытку.`);
+    }
+}
+
+// -------------------------- generate the markup
+async function generateLibraryMarkup() {
+    const moviesPerPage = 20;                                                                    // items per page
+    const numberOfPages = Math.ceil(usersList.length / moviesPerPage);                          // total number of pages
+    let currentPage = 1;                                                                        // current page for markup
+    let startItemPosition = (currentPage - 1) * moviesPerPage;                                  // first item in list for murkup
+    let onePageList = usersList.slice(startItemPosition, startItemPosition + moviesPerPage);    // get list of movie's id for current page
+    galleryLibrary.innerHTML = "";                                                              // clear field
+
+    onePageList = await makeArreyOfDataMovies(onePageList);                                     // making array of the movie objects = movies id list 
+    galleryLibrary.insertAdjacentHTML('beforeend', itemsTemplate(onePageList));                 // set markup
+}
+
+// -------------------------- making movies data object array
+async function makeArreyOfDataMovies(array) {
+    const arr = await Promise.all(array.map(async (el) => {
+        el = await axios.get(`${MOVIE_URL}${el}`, { params: { api_key: API_KEY, language: 'ru-RU' } }); // fetch data by movie id
+        el = el.data;                                                                                   
+        el.name = el.title;                                                                             // ajust data for the template
+        el.release = el.release_date?.slice(0, 4) || 2022;
+        el.genres = el.genres.map(item => item = item.name);                                            // convert genres data object to genres array
+        return el;
+    }
+    ))
+    return Promise.resolve(arr);
+}
