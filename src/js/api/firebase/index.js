cdf3f4e4fe4b6d0f9f2339ae8da539f5e7da942c
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-06-14 22:55:40 +0300

    Update index.js

--- src/js/api/firebase/index.js
+++ src/js/api/firebase/index.js
@@ -3,8 +3,8 @@ import add from './firebase_write_db';
 
 import get from './firebase_read_db';
 import './auth-email';
-import AuthGithub from './auth-github';
+import './auth-github';
 
 
 // export default { logInByGoogle, logOut, addDocument, getDocument, removeFromQueue, addToQueue, addToWatched, getUsersMovieList };
-export default { auth, add, get, AuthGithub }
+export default { auth, add, get }
